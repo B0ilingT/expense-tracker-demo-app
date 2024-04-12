f045998b39970301a80154d0de4ea43577c51315
Tom <121681434+B0ilingT@users.noreply.github.com>
2024-04-12 12:27:40 +0100

    Added Cancel Button

--- Flutter Projects/TestFinance/expense_tracker/lib/widgets/new_expense.dart
+++ Flutter Projects/TestFinance/expense_tracker/lib/widgets/new_expense.dart
@@ -10,20 +10,49 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [
           TextField(
+            controller: _titleController,
+            enableSuggestions: true,
             maxLength: 50,
-            decoration: InputDecoration(
-              label: Text("Title")
+            decoration: const InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: const TextInputType.numberWithOptions(
+              decimal: true,
             ),
-          )
-        ]
-      )
-    );
+            maxLength: 50,
+            decoration:
+                const InputDecoration(label: Text("Amount"), prefixText: "£ "),
+          ),
+          Row(children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel")),
+            ElevatedButton(
+              onPressed: () {
+                print("Title: " + _titleController.text);
+                print("Amount: " + _amountController.text);
+              },
+              child: const Text('Save Expense'),
+            ),
+          ])
+        ]));
   }
 }
